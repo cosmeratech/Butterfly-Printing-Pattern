46ac99c4aae7951b5a9d3d2db03aa12c62317f6b
Janvi Wadhwani <janviwadhwani332@gmail.com>
2025-10-25 18:19:43 +0530

    Add files via upload

---
+++ ButterflyPrintingPattern.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter the Number of rows : ";
+    cin >> rows;
+    cout << "Butterfly Pattern of " << rows << " rows." << endl;
+
+    // Upper part of butterfly
+    for (int i = 0; i <= rows; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+
+        int spaces = 2 * (rows - i);
+        for (int j=0; j < spaces; j++) {
+            cout<<" ";
+        }
+
+        for (int j = 0; j <= i; j++) {
+            cout<<"*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower part of butterfly
+    for (int i = rows - 1; i >= 0; i--) {
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+
+        int spaces = 2 * (rows - i);
+        for (int j = 0; j < spaces; j++) {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
